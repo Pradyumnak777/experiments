f4fb152b151a6a4161192b1eccf319d213830e53
pradyumna <pradyumna.kunchala@gmail.com>
2026-03-12 12:16:52 -0400

    train code added

--- DiffTrack/point_sampling/model.py
+++ DiffTrack/point_sampling/model.py
@@ -1,2 +1,67 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
+class RepMask(nn.Module):
+    def __init__(self, embed_dim = 128): #here val_dim is flow(2) + depth(1) 
+        super(RepMask, self).__init__()
+        
+        self.proj_q = nn.Linear(384, embed_dim)  #this is dino
+        self.proj_kv = nn.Linear(3, embed_dim) # (flow + depth)
+        self.cross_attn = nn.MultiheadAttention(
+            embed_dim=embed_dim, 
+            num_heads=4, 
+            batch_first=True
+        )
+        
+        self.proj_out = nn.Linear(embed_dim, 387) #output from attn_layer is embed_dim dimension, this is for projecting back
+        
+        
+    def forward(self, x):
+        # x input: [B(batch), 387(384+2+1), T, H, W]
+        b, c, t, h, w = x.shape
+        summary = torch.mean(x, dim=(2, 3, 4)) #HOW EFFECTIVE IS THIS??(check online..)
+        dino_part = summary[:, :384]
+        phys_part = summary[:, 384:]
+        
+        Q = self.proj_q(dino_part).unsqueeze(1) #[B, 1, 384] -> [B, 1, 128]
+        KV = self.proj_kv(phys_part).unsqueeze(1) #[B, 1, 3] -> [B 1, 128]
+        
+        attn_out, weights = self.cross_attn(query=Q, key=KV, value=KV)
+        
+        channel_weights = self.proj_out(attn_out.squeeze(1)) #[][B, 387]
+        channel_weights = torch.sigmoid(channel_weights).view(b, 387, 1, 1, 1)
+        
+        return x * channel_weights, weights
+        
+
+class MaskGen(nn.Module):
+    def __init__(self):
+        super(MaskGen, self).__init__()
+        
+        self.attention_block = RepMask(embed_dim=128)
+        
+        self.temporal_conv = nn.Conv3d(in_channels=387, out_channels=64, kernel_size=(8, 1, 1)) #collapsing the clip of 387 channels
+        self.dec_conv1 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
+        self.final_conv = nn.Conv2d(in_channels=32, out_channels=1, kernel_size=1) #this is the final mask
+        
+    def forward(self, x):
+        #get clean features using cross attention above
+        cleaned_x, attn_weights = self.attention_block(x)
+        
+        # new output- [B, 64, 1, 224, 224] #one frame of 64 channels
+        x_2d = self.temporal_conv(cleaned_x)
+        
+        #remove time
+        x_2d = x_2d.squeeze(2) 
+        
+        #new output- [B, 1, 32, 224, 224]
+        x_2d = F.relu(self.dec_conv1(x_2d))
+        
+        #final output- [B, 1, 224, 224]
+        mask_logits = self.final_conv(x_2d)
+        
+        final_mask = torch.sigmoid(mask_logits)
+        
+        return final_mask, attn_weights
---
+++ DiffTrack/point_sampling/train.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from model import MaskGen, RepMask
+from data_utils import UCFRep_train #the custom dataset
+
+def train():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    dataset = UCFRep_train("ucfrep_intermediate_dataset", clip_len=8, k_gap=5)
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=True, drop_last=True)
+    model = MaskGen().to(device)
+    model = MaskGen().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+    temperature = 0.07
+    model.train()
+    for epoch in range(10):
+        for batch_idx, batch in enumerate(dataloader):
+            optimizer.zero_grad()
+            
+            # dataloader output- [B, T=8, C=387, H=224, W=224]
+            # I need [B, C, T, H, W]. So permute:
+            anchor = batch["anchor"].permute(0, 2, 1, 3, 4).to(device)
+            positive = batch["positive"].permute(0, 2, 1, 3, 4).to(device)
+            B = anchor.size(0)
+            
+            #call model for both
+            anchor_mask, _ = model(anchor)
+            pos_mask, _ = model(positive)
+            
+    
+            #dino features needed for infoNCE loss (it is the main comparison)
+            anchor_dino = anchor[:, :384, 0, :, :] # [B, 384, 224, 224]
+            pos_dino = positive[:, :384, 0, :, :]  # [B, 384, 224, 224]
+            
+            #1D vector is needed [B, 384]..multiplying with generated mask and averaging (IS THIS EFFECTIVE??)
+            #add 1e-8 to avoid dividing by zero if the mask is empty
+            anchor_vec = torch.sum(anchor_dino * anchor_mask, dim=(2, 3)) / (torch.sum(anchor_mask, dim=(2, 3)) + 1e-8)
+            pos_vec = torch.sum(pos_dino * pos_mask, dim=(2, 3)) / (torch.sum(pos_mask, dim=(2, 3)) + 1e-8)
+            
+            #normalize
+            anchor_vec = F.normalize(anchor_vec, dim=1)
+            pos_vec = F.normalize(pos_vec, dim=1)
+            
+            '''
+            infoNCE loss step
+            1. TEMPORAL negs- between different batches (A2, P2, A3,..; if current is A1-P1, look at kindle for clarity)
+            2. SPATIAL negs- backgorund mask of same image + background of positive image
+            '''
+            temporal_logits = torch.matmul(anchor_vec, pos_vec.T) / temperature
+            
+            #for anchor background
+            anchor_bg_mask = 1.0 - anchor_mask
+            anchor_bg_vec = torch.sum(anchor_dino * anchor_bg_mask, dim=(2, 3)) / (torch.sum(anchor_bg_mask, dim=(2, 3)) + 1e-8)
+            anchor_bg_vec = F.normalize(anchor_bg_vec, dim=1)
+            anchor_spatial_logits = torch.sum(anchor_vec * anchor_bg_vec, dim=1, keepdim=True) / temperature # [B, 1]
+            
+            #for positive backgoirund
+            pos_bg_mask = 1.0 - pos_mask
+            pos_bg_vec = torch.sum(pos_dino * pos_bg_mask, dim=(2, 3)) / (torch.sum(pos_bg_mask, dim=(2, 3)) + 1e-8)
+            pos_bg_vec = F.normalize(pos_bg_vec, dim=1)
+            pos_spatial_logits = torch.sum(anchor_vec * pos_bg_vec, dim=1, keepdim=True) / temperature # [B, 1]
+
+            
+            #combine them 
+            logits = torch.cat([temporal_logits, anchor_spatial_logits, pos_spatial_logits], dim=1)
+            
+            labels = torch.arange(B).to(device)
+            
+            #pushes the diagonal to 1 and the off-diagonals (negatives) to 0
+            loss = F.cross_entropy(logits, labels)
+            
+            loss.backward()
+            optimizer.step()
+            
+            if batch_idx % 5 == 0:
+                print(f"Epoch: {epoch}, Batch: {batch_idx}, Loss: {loss.item():.4f}")
+                
+if __name__ == "__main__":
+    train()
